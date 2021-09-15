beb67a1760aa3f3fc7dd6048c7e28f34a8d2219e
Raimeeab <raimee.abbassi@gmail.com>
2021-09-15 19:40:33 +1000

    debug highscore sorting

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,27 +1,30 @@
 // ---------------------------- Global Variables ------------------------------
 var scoresRecorded = document.getElementById("scoresRecorded");
-var userInfo = document.createElement("tr");
-var userName = document.createElement("td");
-var userScore = document.createElement("td");
 var clearScores = document.getElementById("clearHistory");
 
 // ---------------------------- Display Scores ---------------------------
 let scores = JSON.parse(localStorage.getItem("Highscores"));
-console.log(scores);
 
-scores.sort(function(a, b) {
-    return parseFloat(a.score) - parseFloat(b.score);
-});
 
 scores.forEach(function (score) {
+    var userInfo = document.createElement("tr");
+    var userName = document.createElement("td");
+    var userScore = document.createElement("td");
     userName.innerHTML = score.userName;
     userScore.innerHTML = score.score;
     scoresRecorded.appendChild(userInfo);
     userInfo.append(userName, userScore);
 });
 
+// ---------------------------- Sort Scores ---------------------------
+scores.sort(function(a, b) {
+    return(parseFloat(a.score) - parseFloat(b.score));
+});
+
+
 // ---------------------------- Clear Scores ---------------------------
 
 clearScores.addEventListener("click", function(){
     window.localStorage.clear("Highscores");
+    userInfo.remove(userName, userScore);
 });
